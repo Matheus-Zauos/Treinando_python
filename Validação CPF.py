31c249ed37bd3db97d3dd682a1e4817dffed00f0
Matheus-Zauos <156464616+Matheus-Zauos@users.noreply.github.com>
2024-01-16 21:41:28 -0300

    Update Validação CPF.py
    Atualização simples buscando práticas do PEP 8

--- Validação CPF.py
+++ Validação CPF.py
@@ -8,16 +8,25 @@ while True:
     soma_cpf_sem_um_dgto = 0
     som_cpf_com_um_dgto = 0
 
-    #Faz a tratativa da string de input
-    cpf_filtrado = re.sub(r'[^0-9]', '', cpf_original)
+    #Faz a tratativa da string de input verificando se está entre 0 e 9
+    cpf_filtrado = re.sub(r'[^0-9]',
+                           '',
+                        cpf_original)
+    
     tamanho_da_string = len(cpf_filtrado)
+
     if tamanho_da_string == 0: cpf_filtrado = '123456789' 
+
     #Verifica se não há repetição de numeros
     primeiro_num_cpf = cpf_filtrado[0]
     busca_repeticao = primeiro_num_cpf * len(cpf_filtrado)
-    if busca_repeticao == cpf_filtrado: cpf_filtrado = '123456789'
+
+    if busca_repeticao == cpf_filtrado: 
+        cpf_filtrado = '123456789'
+
     cpf_sem_digitos = cpf_filtrado[:9]
     cpf_com_um_digito = cpf_filtrado[:10]
+    
     #Executa o cálculo de multiplicação onde x = somat (n * y-1) sendo y = 10.
     for numero in cpf_sem_digitos:
         soma_cpf_sem_um_dgto = soma_cpf_sem_um_dgto + int(numero) * multiplicadorx10
@@ -42,5 +51,7 @@ while True:
     if digito2_calculado <= 9: digito2_esperado = digito2_calculado         
     else: digito2_esperado = 0
           
-    if digito1_esperado == int(cpf_filtrado[-2]) and digito2_esperado == int(cpf_filtrado[-1]): print("Este CPF é válido")           
+    if (digito1_esperado == int(cpf_filtrado[-2]) 
+        and digito2_esperado == int(cpf_filtrado[-1])): 
+        print("Este CPF é válido")           
     else:  print("Este CPF é inválido")    
